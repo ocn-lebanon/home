0e9e2ab22b9534db7d3d94ef012795c1c1c84f39
ocn-lebanon <123776651+ocn-lebanon@users.noreply.github.com>
2023-02-01 11:04:58 +0200

    Add files via upload

---
+++ start.js
@@ -0,0 +1,72 @@
+let startbtn= document.getElementById('start');
+let arabbtn= document.getElementById('arab');
+let engbtn= document.getElementById('eng');
+let lan_text=document.getElementById("lan-elem")
+
+startbtn.addEventListener('click',start);
+
+
+
+
+function start(){
+setTimeout(circle_motions,300)
+setTimeout(ff,550)
+
+
+}
+
+
+function circle_motions() {
+    arabbtn.classList.add("arabic_expand_btn");
+    engbtn.classList.add("eng_expand_btn");
+
+
+  }
+  function circle_motions_remove() {
+    arabbtn.classList.remove("arabic_expand_btn");
+    engbtn.classList.remove("eng_expand_btn");
+
+
+  }
+
+  function ff( ) {
+    arabbtn.classList.add("arabic_expand_btn2");
+    engbtn.classList.add("eng_expand_btn2");
+    startbtn.classList.add("startbtnexpand")
+    lan_text.classList.add("language-box-show")
+
+
+
+
+
+    }
+
+    function ff_remove( ) {
+      arabbtn.classList.remove("arabic_expand_btn2");
+      engbtn.classList.remove("eng_expand_btn2");
+      startbtn.classList.remove("startbtnexpand")
+      lan_text.classList.remove("language-box-show")
+  
+  
+  
+  
+  
+      }
+
+
+function runstart() {
+ 
+  setTimeout(circle_motions,50)
+setTimeout(ff,150)
+startbtn.classList.add("startbtnexpand")
+  
+}
+
+function reestart() {
+  startbtn.classList.remove("startbtnexpand")
+  ff_remove( )
+  circle_motions_remove()
+
+
+  
+}
