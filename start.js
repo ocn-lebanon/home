acfac5e178a76af28b0c1b05513cc100cf7e997f
ocn-lebanon <123776651+ocn-lebanon@users.noreply.github.com>
2023-02-16 19:43:29 +0200

    Add files via upload

--- start.js
+++ start.js
@@ -1,62 +1,24 @@
-let startbtn= document.getElementById('start');
-let arabbtn= document.getElementById('arab');
-let engbtn= document.getElementById('eng');
-let lan_text=document.getElementById("lan-elem")
-let body=document.getElementById("body_id")
+let startbtn = document.getElementById("start");
+let arabbtn = document.getElementById("arab");
+let engbtn = document.getElementById("eng");
+let lan_text = document.getElementById("lan-elem");
+let body = document.getElementById("body_id");
 
-startbtn.addEventListener('click',start);
-body.addEventListener('click',leave)
-
-
-
-function start(){
-setTimeout(circle_motions,300)
-setTimeout(ff,550)
-
-
+function start() {
+  // setTimeout(circle_motions,300)
+  setTimeout(lang_animate, 550);
+  setTimeout(arab_btn_animate, 750);
+  setTimeout(eng_btn_animate, 1000);
 }
 
+function lang_animate() {
+  lan_text.classList.add("lang_box_animation");
+}
 
-function circle_motions() {
-    arabbtn.classList.add("arabic_expand_btn");
-    engbtn.classList.add("eng_expand_btn");
-
-
-  }
-  function circle_motions_remove() {
-    arabbtn.classList.remove("arabic_expand_btn");
-    engbtn.classList.remove("eng_expand_btn");
-
-
-  }
-
-  function ff( ) {
-    arabbtn.classList.add("arabic_expand_btn2");
-    engbtn.classList.add("eng_expand_btn2");
-    startbtn.classList.add("startbtnexpand")
-    lan_text.classList.add("language-box-show")
-
-
-
-
-
-    }
-
-    function ff_remove( ) {
-      arabbtn.classList.remove("arabic_expand_btn2");
-      engbtn.classList.remove("eng_expand_btn2");
-      startbtn.classList.remove("startbtnexpand")
-      lan_text.classList.remove("language-box-show")
-  
-  
-  
-  
-  
-      }
-
-
-    function leave() {
-      setTimeout(ff_remove,100)
-      setTimeout(circle_motions_remove,250)
-
-    }  
+function arab_btn_animate() {
+  arabbtn.classList.add("arab");
+}
+function eng_btn_animate() {
+  engbtn.classList.add("eng");
+}
+start();
